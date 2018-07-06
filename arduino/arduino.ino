261848a28dae76c27b1659fa96156d8bac8080a8
Danial Viltefjall <viltefjall@gmail.com>
2018-07-07 00:51:12 +0200

    removed debug prints and did some code cleanup

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,16 +1,21 @@
-#include <SoftwareSerial.h>
 #include <Adafruit_NeoPixel.h>
 
 /* ---------------------------------------------------------------------- LED */
-#define PIN_LED   (  7)
-#define LED_COUNT (  1)
-#define LED_DIM_T (500)
-#define LED_DELTA ( 10)
+#define PIN_LED0  (  7)
+#define PIN_LED1  (  8)
+#define LED_BRGHT ( 64) // led brightness
+#define LED_COUNT (  1) // number of leds on strips
+#define LED_DIM_T (500) // duration of fade in millis
+#define LED_DELTA ( 10) // delay in millis between pixel changes when fading
 #define LED_STEPS (LED_DIM_T/LED_DELTA)
 
-Adafruit_NeoPixel _led = Adafruit_NeoPixel(LED_COUNT,
-                                           PIN_LED,
-                                           NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel _led0 = Adafruit_NeoPixel(LED_COUNT,
+                                            PIN_LED0,
+                                            NEO_GRB + NEO_KHZ800);
+
+Adafruit_NeoPixel _led1 = Adafruit_NeoPixel(LED_COUNT,
+                                            PIN_LED1,
+                                            NEO_GRB + NEO_KHZ800);
 
 /* -------------------------------------------------------------- hall sensor */
 #define PIN_IRQ (3)
@@ -19,38 +24,29 @@ bool _interrupted = false;
 
 /* ---------------------------------------------------------------------- led */
 void led() {
-  uint8_t cur_r;
-  uint8_t cur_g;
-  uint8_t cur_b;
-
-  uint8_t nxt_r;
-  uint8_t nxt_g;
-  uint8_t nxt_b;
-
-  uint8_t r_delta;
-  uint8_t g_delta;
-  uint8_t b_delta;
-
-  uint32_t cur;
-
   if(_interrupted) {
+    uint8_t cur_r;
+    uint8_t cur_g;
+    uint8_t cur_b;
+
+    uint8_t nxt_r;
+    uint8_t nxt_g;
+    uint8_t nxt_b;
+
+    uint8_t r_delta;
+    uint8_t g_delta;
+    uint8_t b_delta;
+
+    uint32_t cur;
+    
     nxt_r = random(255);
     nxt_g = random(255);
     nxt_b = random(255);
 
-    // nxt_r = 255;
-    // nxt_g = 255;
-    // nxt_b = 255;
-
-    cur    = _led.getPixelColor(0);
+    cur    = _led0.getPixelColor(0);
     cur_r  = (cur & 0xff0000) >> 16;
     cur_g  = (cur & 0x00ff00) >>  8;
     cur_b  = (cur & 0x0000ff) >>  0;
-
-    Serial.print("c: "); Serial.println(cur);
-    Serial.print("r: "); Serial.println(cur_r);
-    Serial.print("g: "); Serial.println(cur_g);
-    Serial.print("b: "); Serial.println(cur_b);
     
     r_delta = cur_r / LED_STEPS;
     g_delta = cur_g / LED_STEPS;
@@ -62,14 +58,13 @@ void led() {
       cur_g -= g_delta;
       cur_b -= b_delta;
       
-      _led.setPixelColor(0, cur_r, cur_g, cur_b);
-      _led.show();
+      _led0.setPixelColor(0, cur_r, cur_g, cur_b);
+      _led0.show();
       delay(LED_DELTA);
     }
 
-    _led.setPixelColor(0, 0, 0, 0);
-    _led.show();
-    Serial.println("dimmed down");
+    _led0.setPixelColor(0, 0, 0, 0);
+    _led0.show();
 
     // dim up, re-use variables
     cur_r = cur_g = cur_b = 0;
@@ -83,17 +78,13 @@ void led() {
       cur_g += g_delta;
       cur_b += b_delta;
       
-      _led.setPixelColor(0, cur_r, cur_g, cur_b);
-      _led.show();
+      _led0.setPixelColor(0, cur_r, cur_g, cur_b);
+      _led0.show();
       delay(LED_DELTA);
     }
 
-    _led.setPixelColor(0, nxt_r, nxt_g, nxt_b);
-    _led.show();
-    Serial.print("r: "); Serial.println(nxt_r);
-    Serial.print("g: "); Serial.println(nxt_g);
-    Serial.print("b: "); Serial.println(nxt_b);
-    Serial.println("dimmed up");
+    _led0.setPixelColor(0, nxt_r, nxt_g, nxt_b);
+    _led0.show();
     
     _interrupted = false;
   }
@@ -101,10 +92,9 @@ void led() {
 
 /* -------------------------------------------------------------------- setup */
 void setup() {
-  Serial.begin(9600);
-  _led.begin();
-  _led.setBrightness(64);
-  _led.show();
+  _led0.begin();
+  _led0.setBrightness(LED_BRGHT);
+  _led0.show();
 
   attachInterrupt( digitalPinToInterrupt(PIN_IRQ), irq, FALLING );
   pinMode(PIN_IRQ, INPUT_PULLUP);
@@ -114,14 +104,13 @@ void setup() {
 
 /* --------------------------------------------------------------------- loop */
 void loop() {
+  led();
   delay(10);
 }
 
 /* ---------------------------------------------------------------------- irq */
 void irq() {
-  Serial.println("Irq!");
   if(!_interrupted) {
-    Serial.println("interrupted = true");
     _interrupted = true;
   }
 }
